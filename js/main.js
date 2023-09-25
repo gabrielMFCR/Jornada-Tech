da69b647cf462f4a68a378f3422cf7d57e2c8a12
Gabriel <gabrielmfcr@gmail.com>
2023-09-25 15:31:06 -0300

    commit_validate

--- js/main.js
+++ js/main.js
@@ -4,6 +4,47 @@ $(document).ready(function(){
     $('#cpf').mask('000.000.000-00')
 
     $('#cep').mask('00000-000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome.',
+            email: 'Por favor, insira seu email',
+            telefone: 'Por favor, insira seu número de telefone',
+            cpf: 'Por favor, insira seu cpf',
+            endereco: 'Por favor, insira seu endereço',
+            cep: 'Por favor, insira seu cep'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
 });
     
 
